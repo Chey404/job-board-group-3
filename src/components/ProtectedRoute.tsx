0c1290fe9bac49f5e89dcb4cf310a747401fd4b2
Andy-Vu-1229 <98198637+Andy-Vu-1229@users.noreply.github.com>
2025-10-26 15:03:19 -0400

    Update ProtectedRoute.tsx
    forgot import

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
 
 interface ProtectedRouteProps {
